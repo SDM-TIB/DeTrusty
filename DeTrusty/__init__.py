84baf6bed6c0632a1b9f995859648a787f3ffc69
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-02-14 10:18:27 +0100

    add flexibility to get_logger

--- DeTrusty/__init__.py
+++ DeTrusty/__init__.py
@@ -1,10 +1,13 @@
 import logging
 
 
-def get_logger(name):
+def get_logger(name, filename=None, level=logging.INFO):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
+    logger.setLevel(level)
+    if filename is None:
+        handler = logging.StreamHandler()
+    else:
+        handler = logging.FileHandler(filename)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
