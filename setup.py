37ec0804d54328c6050a64378e09b6b3b59a37e6
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-21 15:00:44 +0200

    update setup.py
    in preparation for publishing DeTrusty as a library

--- setup.py
+++ setup.py
@@ -4,21 +4,51 @@ import setuptools
 with open("VERSION", "r", encoding="utf-8") as ver:
     version = ver.read()
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='DeTrusty',
     version=version,
     description='DeTrusty - Decentralized and Trustable Query Engine',
+    license='GNU/GPLv3',
     author='Philipp D. Rohde',
     author_email='philipp.rohde@tib.eu',
+    url='https://github.com/SDM-TIB/DeTrusty',
+    download_url='https://github.com/SDM-TIB/DeTrusty/archive/refs/tags/v' + version + '.tar.gz',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     scripts=['./Scripts/create_rdfmts.py',
              './Scripts/restart_workers.sh'],
-    package_dir={"": "DeTrusty"},
-    packages=setuptools.find_packages(where="DeTrusty"),
+    packages=[
+        'DeTrusty',
+        'DeTrusty.Decomposer',
+        'DeTrusty.Molecule',
+        'DeTrusty.Operators',
+        'DeTrusty.Operators.AnapsidOperators',
+        'DeTrusty.Operators.BlockingOperators',
+        'DeTrusty.Operators.NonBlockingOperators',
+        'DeTrusty.Sparql',
+        'DeTrusty.Sparql.Parser',
+        'DeTrusty.Wrapper',
+        'DeTrusty.Wrapper.RDFWrapper'
+    ],
     install_requires=['requests==2.27.1',
                       'Flask==2.0.3',
                       'ply==3.11'],
     include_package_data=True,
-    license='GNU/GPL v3',
     python_requires='>=3.8',
-    zip_safe=False
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research'
+    ]
 )
