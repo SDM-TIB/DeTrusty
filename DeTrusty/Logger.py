5844a29648a7c64c3eba0986f8660cd9126d42f9
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-07-28 13:50:44 +0200

    fix loggers

--- DeTrusty/Logger.py
+++ DeTrusty/Logger.py
@@ -12,9 +12,9 @@ def get_logger(name, filename=None, level=logging.INFO, file_and_console=False):
         file_handler.setLevel(level)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-        if filename is None or file_and_console:
-            console_handler = logging.StreamHandler()
-            console_handler.setLevel(level)
-            console_handler.setFormatter(formatter)
-            logger.addHandler(console_handler)
+    if filename is None or file_and_console:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
     return logger
