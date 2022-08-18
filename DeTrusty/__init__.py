b1d09e0180c434ed464c65b1cfeb75474beb29ad
Philipp Rohde <Philipp.Rohde@tib.eu>
2022-08-18 11:50:33 +0200

    add docstring for run_query()

--- DeTrusty/__init__.py
+++ DeTrusty/__init__.py
@@ -19,6 +19,54 @@ def run_query(query: str,
               join_stars_locally: bool = True,
               print_result: bool = True,
               yasqe: bool = False):
+    """Executes a SPARQL query over a federation of SPARQL endpoints.
+
+    The SPARQL query is decomposed based on the specified decomposition type.
+    DeTrusty identifies the possible sources for each sub-query using the
+    metadata collected previously. If the query contains the SERVICE clause,
+    DeTrusty executed the sub-query at the specified endpoint instead.
+
+    Parameters
+    ----------
+    query : str
+        The SPARQL query to be executed.
+    decomposition_type : str
+        The decomposition type to be used for decomposing the query. Possible values
+        are 'STAR' for a star-shaped decomposition, 'EG' for exclusive groups decomposition,
+        and 'TRIPLE' for a triple-wise decomposition, i.e., each triple pattern of the query
+        produces a sub-query. Default is 'STAR'.
+    sparql_one_dot_one : bool
+        Indicates whether the query includes the SERVICE clause.
+        'True' meaning the SERVICE clause is present in the query, 'False' otherwise.
+        Default is 'False'.
+    config : DeTrusty.Molecule.MTManager.ConfigFile
+        The configuration holding the metadata about the federation over which the
+        SPARQL query should be executed. If no value is specified, DeTrusty will
+        attempt to load the configuration from `./Config/rdfmts.json`.
+    join_stars_locally : bool
+        Indicates whether joins should be performed at the query engine.
+        'True' meaning joins will be performed in DeTrusty, 'False' leads to joins
+        being executed in the sources if possible. Default behavior is join execution
+        at the query engine level.
+    print_result : bool
+        Indicates whether the actual query result should be returned.
+        'True' meaning the result will be included in the answer.
+        'False' only returns the metadata of the query result, like the cardinality.
+        Default is 'True'.
+    yasqe : bool
+        Indicates whether the SPARQL query was sent from the YASGUI interface of
+        DeTrusty's Web interface. This is a workaround for YASQE not being able
+        to show the query results when the validation data is included.
+        Set to 'True' to omit the validation data. Default is 'False'.
+
+    Returns
+    -------
+    dict
+        A dictionary including the query answer and additional metadata following the SPARQL protocol.
+        It returns an error message in the 'error' field if something went wrong. Other metadata might
+        be omitted in that case.
+
+    """
     start_time = time.time()
     decomposer = Decomposer(query, config,
                             decompType=decomposition_type,
