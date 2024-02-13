ee73083c965916ed6ebc0418e88e558d0d680e71
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-02-13 13:36:56 +0100

    use stream handler for loggers if file handler cannot be established

--- DeTrusty/Logger.py
+++ DeTrusty/Logger.py
@@ -8,10 +8,13 @@ def get_logger(name, filename=None, level=logging.INFO, file_and_console=False):
     logger.setLevel(level)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     if filename is not None:
-        file_handler = logging.FileHandler(filename)
-        file_handler.setLevel(level)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        try:
+            file_handler = logging.FileHandler(filename)
+            file_handler.setLevel(level)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+        except (FileNotFoundError, PermissionError):
+            file_and_console = True  # if file handler cannot be established, use stream handler
     if filename is None or file_and_console:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
