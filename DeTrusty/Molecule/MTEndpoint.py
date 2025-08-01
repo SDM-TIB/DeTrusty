655d075eef66bd129002e0f5b4db310b57924277
Philipp Rohde <Philipp.Rohde@tib.eu>
2025-08-01 15:48:02 +0200

    move update queries into MTEndpoint

--- DeTrusty/Molecule/MTEndpoint.py
+++ DeTrusty/Molecule/MTEndpoint.py
@@ -11,9 +11,7 @@ from pyoxigraph import serialize as oxi_serialize
 from requests.auth import HTTPDigestAuth
 
 from DeTrusty.Logger import get_logger
-from DeTrusty.Molecule import SEMSD, get_query_delete_property_range, get_query_delete_source_from_property, \
-    get_query_delete_property_no_source, get_query_delete_source_from_class, get_query_delete_class_no_source, \
-    get_query_delete_source
+from DeTrusty.Molecule import SEMSD
 from DeTrusty.Wrapper.RDFWrapper import contact_source
 
 logger = get_logger(__name__)
@@ -62,6 +60,95 @@ class MTEndpoint(object):
     @abc.abstractmethod
     def delete_endpoint(self, endpoint: str):
         pass
+
+    @staticmethod
+    def get_query_delete_property_range(endpoint_url: str):
+        return '''
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX semsd: <{semsd}>
+        DELETE {{
+          ?pred semsd:propertyRange ?pr .
+          ?pr ?p ?o .
+        }} WHERE {{
+          ?pred a rdf:Property ;
+            semsd:propertyRange ?pr .
+          ?pr a semsd:PropertyRange ;
+            semsd:hasSource <{url}> ;
+            ?p ?o .
+        }}
+        '''.format(semsd=SEMSD, url=endpoint_url)
+
+    @staticmethod
+    def get_query_delete_source_from_property(endpoint_url: str):
+        return '''
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX semsd: <{semsd}>
+        DELETE {{
+          ?pred semsd:hasSource <{url}> .
+        }} WHERE {{
+          ?pred a rdf:Property ;
+            semsd:hasSource <{url}> .
+        }}
+        '''.format(semsd=SEMSD, url=endpoint_url)
+
+    @staticmethod
+    def get_query_delete_property_no_source():
+        return '''
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX semsd: <{semsd}>
+        DELETE {{
+          ?c semsd:hasPredicate ?pred .
+          ?pred ?p ?o .
+        }} WHERE {{
+          ?pred a rdf:Property ;
+            ?p ?o .
+          FILTER NOT EXISTS {{ ?pred semsd:hasSource ?source }}
+          ?c a rdfs:Class ;
+            semsd:hasProperty ?pred .
+        }}
+        '''.format(semsd=SEMSD)
+
+    @staticmethod
+    def get_query_delete_source_from_class(endpoint_url: str):
+        return '''
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX semsd: <{semsd}>
+        DELETE {{
+          ?c semsd:hasSource <{url}> .
+        }} WHERE {{
+          ?c a rdfs:Class ;
+            semsd:hasSource <{url}> .
+        }}
+        '''.format(semsd=SEMSD, url=endpoint_url)
+
+    @staticmethod
+    def get_query_delete_class_no_source():
+        return '''
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX semsd: <{semsd}>
+        DELETE {{
+          ?c ?p ?o .
+        }} WHERE {{
+          ?c a rdfs:Class ;
+            ?p ?o .
+          FILTER NOT EXISTS {{ ?c semsd:hasSource ?source }}
+        }}
+        '''.format(semsd=SEMSD)
+
+    @staticmethod
+    def get_query_delete_source(endpoint_url: str):
+        return '''
+        PREFIX semsd: <{semsd}>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        DELETE {{
+          ?source ?p ?o .
+        }} WHERE {{
+          ?source a semsd:DataSource ;
+            semsd:hasURL "{url}"^^xsd:anyURI ;
+            ?p ?o .
+        }}
+        '''.format(semsd=SEMSD, url=endpoint_url)
 
 
 class SPARQLEndpoint(MTEndpoint):
@@ -169,12 +256,12 @@ class SPARQLEndpoint(MTEndpoint):
             logger.warning('{kg} is not accessible and, hence, cannot be added to the federation.'.format(kg=endpoint.url))
 
     def delete_endpoint(self, endpoint: str):
-        self._update(get_query_delete_property_range(endpoint))
-        self._update(get_query_delete_source_from_property(endpoint))
-        self._update(get_query_delete_property_no_source())
-        self._update(get_query_delete_source_from_class(endpoint))
-        self._update(get_query_delete_class_no_source())
-        self._update(get_query_delete_source(endpoint))
+        self._update(self.get_query_delete_property_range(endpoint))
+        self._update(self.get_query_delete_source_from_property(endpoint))
+        self._update(self.get_query_delete_property_no_source())
+        self._update(self.get_query_delete_source_from_class(endpoint))
+        self._update(self.get_query_delete_class_no_source())
+        self._update(self.get_query_delete_source(endpoint))
 
 
 class PyOxigraphEndpoint(MTEndpoint):
@@ -192,12 +279,12 @@ class PyOxigraphEndpoint(MTEndpoint):
                       prefixes={'semsd': SEMSD})
 
     def delete_endpoint(self, endpoint: str):
-        self.ttl.update(get_query_delete_property_range(endpoint))
-        self.ttl.update(get_query_delete_source_from_property(endpoint))
-        self.ttl.update(get_query_delete_property_no_source())
-        self.ttl.update(get_query_delete_source_from_class(endpoint))
-        self.ttl.update(get_query_delete_class_no_source())
-        self.ttl.update(get_query_delete_source(endpoint))
+        self.ttl.update(self.get_query_delete_property_range(endpoint))
+        self.ttl.update(self.get_query_delete_source_from_property(endpoint))
+        self.ttl.update(self.get_query_delete_property_no_source())
+        self.ttl.update(self.get_query_delete_source_from_class(endpoint))
+        self.ttl.update(self.get_query_delete_class_no_source())
+        self.ttl.update(self.get_query_delete_source(endpoint))
         self.ttl.optimize()
 
     def add_endpoint(self, endpoint: str):
